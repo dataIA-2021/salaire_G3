fa3306c95eda2e3f87c0905e50cb48139c6a2e37
Fdelys <frandel932@hotmail.com>
2022-02-22 11:40:42 +0100

    Update scrapDroite.py
    Modif, mise en forme url automate

--- scrapDroite.py
+++ scrapDroite.py
@@ -8,9 +8,9 @@ Created on Tue Feb 22 10:51:21 2022
 import requests
 from bs4 import BeautifulSoup as BS
 
-url = 'https://fr.indeed.com/voir-emploi?jk=daf6f8c39149c15e'
+url_b = 'https://fr.indeed.com/voir-emploi?jk=%s'%(record[0])
 
-response = requests.get(url)
+response = requests.get(url_b)
 soup = BS(response.text, 'html.parser')
 comment = soup.find('div','jobsearch-jobDescriptionText').text
 # text = comment.find('div')
